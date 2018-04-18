6c47128dd2f8cef90b65a93c0a0375522e5c0f61
reshma-rs-spericorn <reshmars@spericorn>
2018-04-18 11:04:42 +0530

    add all fields display

--- client_src/src/components/TaskDetails.js
+++ client_src/src/components/TaskDetails.js
@@ -38,9 +38,15 @@ class TaskDetails extends Component {
         <Link to="/tasks" className="btn grey">Back</Link>
         <h3> {this.state.details.name}</h3>
         <ul className="collection">
-          <li className="collection-item">start: {this.state.details.startdate}</li>
-          <li className="collection-item">end: {this.state.details.enddate}</li>
-          <li className="collection-item">comment: {this.state.details.comment}</li>
+          <li className="collection-item">Start Date: {this.state.details.startdate}</li>
+          <li className="collection-item">End Date: {this.state.details.enddate}</li>
+          <li className="collection-item">Comment: {this.state.details.comment}</li>
+          <li className="collection-item">Description: {this.state.details.description}</li>
+          <li className="collection-item">Log Time: {this.state.details.logtime}</li>
+          <li className="collection-item">Priority: {this.state.details.priority}</li>
+          <li className="collection-item">Status: {this.state.details.status}</li>
+          <li className="collection-item">Task Assigned: {this.state.details.taskassigned}</li>
+          <li className="collection-item">File: {this.state.details.file}</li>
         </ul>
         <Link className="btn" to={`/tasks/edit/${this.state.details.id}`}>Edit</Link>
         <button onClick={this.onDelete.bind(this)} className="btn red right">Delete</button>
